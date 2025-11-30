8ad8751a40f436212f25307e0df7fe1c91181725
towurzer <t.wurzer.2004@gmail.com>
2025-11-30 05:05:13 +0100

    Hide teammate hud

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,6 +5,13 @@ extends CharacterBody3D
 @export var camera_controller: Node3D
 @export var camera: Camera3D
 
+@export_group("Hud elements")
+@export var point_display: Node3D
+@export var circle_hud: Node3D
+@export var pitch_hud: Node3D
+@export var speed_bar: Node3D
+
+
 func _enter_tree():
 	# 1. Set Authority based on Name (Standard stuff)
 	set_multiplayer_authority(str(name).to_int())
@@ -35,3 +42,8 @@ func _ready():
 		# We do not want to process inputs for other people's players.
 		movement_controller.set_physics_process(false)
 		camera_controller.set_process(true)
+		
+		point_display.hide()
+		circle_hud.hide()
+		pitch_hud.hide()
+		speed_bar.hide()
