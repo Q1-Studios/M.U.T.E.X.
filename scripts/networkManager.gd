f456ff984cb47565e192eee3eb5b20b344da0cd3
Elias Pozewaunig <elias.pozewaunig@aau.at>
2025-12-31 07:23:45 +0100

    Added fallback IP back to joining

--- scripts/networkManager.gd
+++ scripts/networkManager.gd
@@ -2,7 +2,8 @@ extends Node
 
 # NetworkManager.gd
 const PORT = 8080
-var DEFAULT_SERVER_IP = "" # Leave blank initially, last entered IP will be remembered
+var LAST_SERVER_IP = "" # Leave blank initially, last entered IP will be remembered
+var DEFAULT_SERVER_IP = "127.0.0.1"
 
 const LEVEL_SCENE_PATH = "res://scenes/LevelScene.tscn" 
 const CLIENT_CONNECTION_TIMEOUT: float = 3.0
