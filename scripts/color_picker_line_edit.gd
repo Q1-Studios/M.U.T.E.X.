2af98a30d2957e676599788467c013d7ff082ef3
Elias Pozewaunig <elias.pozewaunig@aau.at>
2025-12-31 06:47:16 +0100

    Added interactive color picker to hosting screen

---
+++ scripts/color_picker_line_edit.gd
@@ -0,0 +1,38 @@
+extends LineEdit
+
+@export var color_picker: ColorPicker
+@export var preview: ColorRect
+
+func _ready() -> void:
+	color_picker.hide()
+	color_picker.color = Color(text)
+	preview.color = Color(text)
+	
+	text_changed.connect(_on_text_changed)
+	color_picker.color_changed.connect(_on_color_picker_changed)
+	get_viewport().gui_focus_changed.connect(_on_gui_focus_changed)
+
+func _on_gui_focus_changed(control: Control) -> void:
+	# Color picker consists of multiple internal children
+	# Thus, a simple focus check does not suffice
+	# Otherwise the color picker would be hidden despite being clicked
+	if control == self or get_children_recursive().has(control):
+		color_picker.show()
+	else:
+		color_picker.hide()
+
+func _on_text_changed(new_text: String) -> void:
+	var new_color: Color = Color(new_text)
+	color_picker.color = new_color
+	preview.color = new_color
+
+func _on_color_picker_changed(color: Color) -> void:
+	text = "#" + color.to_html(false)
+	preview.color = color
+
+func get_children_recursive(node: Node = self) -> Array[Node]:
+	var result: Array[Node] = []
+	for child in node.get_children(true):
+		result.append(child)
+		result.append_array(get_children_recursive(child))
+	return result
