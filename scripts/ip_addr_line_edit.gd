7a4dd0c7e3da4e1067f165ab7b0a83bc9ea6fd86
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-01-07 15:59:02 +0100

    Implemented cleaner IP address handling

---
+++ scripts/ip_addr_line_edit.gd
@@ -0,0 +1,30 @@
+extends LineEdit
+
+@export var dependant_btn: Button
+@export var error_label: Label
+
+func _ready() -> void:
+	focus_entered.connect(_on_focus_entered)
+	text_changed.connect(_on_text_changed)
+	_on_text_changed(text)
+
+func _on_focus_entered() -> void:
+	edit()
+	caret_column = text.length()
+
+func _on_text_changed(new_text: String) -> void:
+	if not new_text.is_empty() and is_ip(new_text):
+		dependant_btn.disabled = false
+		error_label.hide()
+	else:
+		dependant_btn.disabled = true
+		error_label.show()
+
+func is_ip(string: String) -> bool:
+	var ip_regex := RegEx.new()
+	ip_regex.compile("^[0-9.]+$")
+
+	if ip_regex.search(string):
+		return true
+	else:
+		return false
--- scripts/join_btn.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@export var ip_field: LineEdit
-
-func _process(_delta: float) -> void:
-	if(ip_field.text != ""):
-		disabled = false
-	else:
-		disabled = true
