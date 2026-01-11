5a54862b92bc886497ab707f1f97ff86d5c8b0bf
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-01-11 01:31:20 +0100

    Allow returning to main menu from tutorial

--- scripts/action_triggered_button.gd
+++ scripts/action_triggered_button.gd
@@ -3,5 +3,5 @@ extends Button
 @export var action: String = "ui_cancel"
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed(action) and is_visible_in_tree():
+	if Input.is_action_just_pressed(action) and get_parent().is_visible_in_tree():
 		pressed.emit()
