0bdd9ccfccffa32653a673d2834afa683a7a7be6
Elias Pozewaunig <elias.pozewaunig@aau.at>
2026-01-17 23:29:41 +0100

    Added sound effects to LineEdits too

--- scripts/button_sound_player.gd
+++ scripts/button_sound_player.gd
@@ -1,16 +1,23 @@
 extends Node
 
-@onready var button: Button = $".."
+@onready var control: Control = $".."
 
 @onready var focus_sound: AudioStreamPlayer = $FocusSound
 @onready var click_sound: AudioStreamPlayer = $ClickSound
 
 func _ready() -> void:
-	button.focus_entered.connect(_on_focus)
-	button.pressed.connect(_on_pressed)
+	control.focus_entered.connect(_on_focus)
+	
+	if control is Button:
+		control.pressed.connect(_on_pressed)
+	if control is LineEdit:
+		control.text_submitted.connect(_on_text_submitted)
 
 func _on_focus() -> void:
 	focus_sound.play()
 
 func _on_pressed() -> void:
+	click_sound.play()
+
+func _on_text_submitted(_new_text: String) -> void:
 	click_sound.play()
