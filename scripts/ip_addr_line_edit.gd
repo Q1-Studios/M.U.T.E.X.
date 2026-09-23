294c6c09f30a5d5c8b5eb7a201b8b358b2f4e942
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-09-23 03:24:11 +0200

    Added support for connecting to IPv6

--- scripts/ip_addr_line_edit.gd
+++ scripts/ip_addr_line_edit.gd
@@ -12,18 +12,27 @@ func _ready() -> void:
 	_on_text_changed(text)
 
 func _on_text_changed(new_text: String) -> void:
-	if not new_text.is_empty() and is_ip(new_text):
+	if not new_text.is_empty() and (is_ipv4(new_text) or is_ipv6(new_text)):
 		dependant_btn.disabled = false
 		error_label.hide()
 	else:
 		dependant_btn.disabled = true
 		error_label.show()
 
-func is_ip(string: String) -> bool:
-	var ip_regex := RegEx.new()
-	ip_regex.compile("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|([1-9])?[0-9])(\\.(?!$)|$)){4}$")
+func is_ipv4(string: String) -> bool:
+	var ipv4_regex := RegEx.new()
+	ipv4_regex.compile("^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|([1-9])?[0-9])(\\.(?!$)|$)){4}$")
 	
-	if ip_regex.search(string):
+	if ipv4_regex.search(string):
+		return true
+	else:
+		return false
+
+func is_ipv6(string: String) -> bool:
+	var ipv6_regex := RegEx.new()
+	ipv6_regex.compile("^((([0-9A-Fa-f]{1,4}:){1,6}:)|(([0-9A-Fa-f]{1,4}:){7}))([0-9A-Fa-f]{1,4})$")
+	
+	if ipv6_regex.search(string):
 		return true
 	else:
 		return false
